d724e8e77325ba17c3ca1ae8895091f822b06071
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2022-09-05 17:01:51 -0300

    Door Open

---
+++ HorrorGamePrototype/Assets/_Scripts/OpenDoor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnMouseDown()
+    {
+        if (anim.GetBool("Open") == false)
+        {
+            anim.SetBool("Open", true);
+            anim.SetBool("Close", false);
+        }
+        else
+        {
+            anim.SetBool("Open", false);
+            anim.SetBool("Close", true);
+        }
+            
+    }
+}
