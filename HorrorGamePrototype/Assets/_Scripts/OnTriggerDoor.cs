10a358f67cd28b140df783110709028ff0e90a03
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2022-09-06 20:56:41 -0300

    Door drag system

---
+++ HorrorGamePrototype/Assets/_Scripts/OnTriggerDoor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerDoor : MonoBehaviour
+{
+    static public bool _OnTriggerDoor {get; private set; }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _OnTriggerDoor = true;
+            Debug.Log(other);
+            Debug.Log(_OnTriggerDoor);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _OnTriggerDoor = false;
+            Debug.Log(other);
+            Debug.Log(_OnTriggerDoor);
+               
+        }
+    }
+}
--- HorrorGamePrototype/Assets/_Scripts/OpenDoor.cs
+++ HorrorGamePrototype/Assets/_Scripts/OpenDoor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
+    
     Animator anim;
     private void Start()
     {
