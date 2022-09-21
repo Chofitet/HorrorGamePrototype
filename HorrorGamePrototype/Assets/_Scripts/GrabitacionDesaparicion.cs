d4c9f6771161cf627cfca4191ef7280358b3995d
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2022-09-21 17:02:50 -0300

    Prototype details

---
+++ HorrorGamePrototype/Assets/_Scripts/GrabitacionDesaparicion.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabitacionDesaparicion : MonoBehaviour
+{
+    [SerializeField] GameObject DesappearObject;
+    [SerializeField] GameObject AppearObject;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            DesappearObject.SetActive(false);
+            AppearObject.SetActive(true);
+        }
+    }
+}
